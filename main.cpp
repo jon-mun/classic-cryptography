be338c742b3ad56dbf2e6557c69293f236cf4373
jon-mun <jonathan.edmund@gmail.com>
2023-02-26 00:28:49 +0700

    add decrypt functionality

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
-// create cpp boiler plate
-
 #include<iostream>
 using namespace std;
+
 enum operation
     {
         SWAP,
         ADD,
         MULTIPLY
     };
-int binaryToDecimal(string n);
+
+struct operationDetails {
+    operation op;
+    int value;
+};
+void operationController(operationDetails details, string& inputText, int index);
+void operationDecryptController(operationDetails details, string& inputText, int index);
 operation getOperation(string operationBlock);
-void operationController(operation op, string& inputText, int value);
+int binaryToDecimal(string n);
 
 int main()
 {
@@ -28,19 +33,40 @@ int main()
     // cin >> inputText;
 
 
-    key = "1000000100000010";
+    key = "}'/+-";
+    // TODO never reaches 1*******
     inputText = "HelloWorld!";
 
+    cout << key << endl;
+    // 0. convert key to 8 bit binary string
+    string binaryKey = "";
+    for (int i = 0; i < key.length(); i++)
+    {
+        int ascii = key[i];
+        string binary = "";
+        while (ascii > 0)
+        {
+            binary = (ascii % 2 == 0 ? "0" : "1") + binary;
+            ascii /= 2;
+        }
+        while (binary.length() < 8)
+        {
+            binary = "0" + binary;
+        }
+        binaryKey += binary;
+    }
+    cout << binaryKey << endl;
+
     // 1. split key into 8 bit blocks
-    int blocks = key.length() / 8;
+    int blocks = binaryKey.length() / 8;
     string key_blocks[blocks];
 
     for (int i = 0; i < blocks; i++)
     {
-        key_blocks[i] = key.substr(i * 8, 8);
+        key_blocks[i] = binaryKey.substr(i * 8, 8);
     }
 
-    string encryptedText = inputText;
+    operationDetails operations[blocks];
     // 2. perform operations on each block
     for (int i = 0; i < blocks; i++)
     {
@@ -53,10 +79,36 @@ int main()
         operation op = getOperation(operationBlock);
         cout << op << endl;
         cout << value << endl;
+        operations[i] = { op, value };
+    }
 
-        // 2.4. perform operation on input text
-       operationController(op, encryptedText, value);
-       cout << encryptedText << " " << encryptedText.length() << endl;
+    // 3. perform operations on inputText
+    string encryptedText = inputText;
+    for (int i = 0; i < blocks; i++)
+    {
+        cout << operations[i].op << " " << operations[i].value << endl;
+        for(int j = 1; j < encryptedText.length(); j++)
+        {
+            operationController(operations[i], encryptedText, j);
+        }
+    }
+    cout << encryptedText << endl;
+
+    for (int i = 0; i < encryptedText.length(); i++)
+    {
+        cout << (int)encryptedText[i] << " ";
+    }
+
+    cout << endl;
+
+    string decryptedText = encryptedText;
+    for (int i = blocks - 1; i >= 0; i--)
+    {   
+        cout << operations[i].op << " " << operations[i].value << endl;
+        for(int j = decryptedText.length()-1; j > 0 ; j--)
+        {
+            operationDecryptController(operations[i], decryptedText, j);
+        }
     }
 
     return 0;
@@ -82,6 +134,23 @@ int binaryToDecimal(string n)
     return dec_value;
 };
 
+
+// function to swap characters
+string swapChar(string inputText, int startIndex, int endIndex)
+{
+    string outputText = inputText;
+    outputText[startIndex] = inputText[endIndex];
+    outputText[endIndex] = inputText[startIndex];
+
+    return outputText;
+}
+
+string swapForward(string inputText, int startIndex, int value) 
+{
+    int endIndex = (startIndex + value) % inputText.length();
+    return swapChar(inputText, startIndex, endIndex);
+}
+
 // function to enumerate operations
 operation getOperation(string operationBlock)
 {
@@ -105,44 +174,49 @@ operation getOperation(string operationBlock)
     }
 }
 
-// function to swap characters
-string swapChar(string inputText, int startIndex, int value)
+void operationController(operationDetails details, string& inputText, int index)
 {
-    int endIndex = startIndex + value % inputText.length();
-
-    string outputText = inputText;
-    outputText[startIndex] = inputText[endIndex];
-    outputText[endIndex] = inputText[startIndex];
-
-    return outputText;
-}
-
-void operationController(operation op, string& inputText, int value)
-{
+    operation op = details.op;
+    int value = details.value;
     switch (op)
     {
     case SWAP:
-        for (int i = 1; i < inputText.length(); i++)
-        {
-            inputText = swapChar(inputText, i, value);
-            cout << inputText << endl;
-        }
+        inputText = swapForward(inputText, index, value);
+        cout << inputText << " " << op << value << endl;
         break;
     case ADD:
-        for (int i = 1; i < inputText.length(); i++)
-        {
-            inputText[i] = (inputText[i] + value) % 256;
-            cout << inputText << endl;
-        }
+        inputText[index] = (inputText[index] + value) % 256;
+        cout << inputText << endl;
         break;
     case MULTIPLY:
-        for (int i = 1; i < inputText.length(); i++)
-        {
-            inputText[i] = (inputText[i] * value) % 256;
-            cout << inputText << endl;
-        }
+        inputText[index] = (inputText[index] * value) % 256;
+        cout << inputText << endl;
         break;
     default:
         break;
     }
 }
+
+void operationDecryptController(operationDetails details, string& inputText, int index)
+{
+    operation op = details.op;
+    int value = details.value;
+    switch (op)
+    {
+    case SWAP:
+        inputText = swapForward(inputText, index, value);
+        cout << inputText << " " << op << " " << value << endl;
+        break;
+    case ADD:
+        inputText[index] = (inputText[index] - value) % 256;
+        cout << inputText << endl;
+        break;
+    case MULTIPLY:
+        inputText[index] = (inputText[index] / value) % 256;
+        cout << inputText << endl;
+        break;
+    default:
+        break;
+    }
+}
+
